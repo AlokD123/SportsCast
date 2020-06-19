7dd27507c5506eb0b00f3ba5d4ec76b063b02cbe
AlokD123 <alok.deshpande@mail.utoronto.ca>
2020-06-19 00:05:58 -0400

    Added DataLoading module to load (and clean) the raw NHL data, as well as possibly save it. All tested except saving

---
+++ SportsCast_Framework/scripts/DataLoading.py
@@ -0,0 +1,154 @@
+
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+import copy
+import glnts
+import pickle
+import pandas as pd
+#from Preprocessing import Preprocessing
+from SavingReading import SavingReading
+
+#ENVIRONMENT VARIABLES
+'''
+PRJ_PATH = 
+DATA_DIR = PRJ_PATH + "/data/inputs"
+DATA_FILENAME = "full_dataset_4_seasons"        #.csv
+UPDATED_DATA_DIR = PRJ_PATH + "/data/inputs"
+UPDATED_DATA_FILENAME = "full_dataset_updated"  #.csv
+ROSTER_DIR = PRJ_PATH + "/data/inputs"
+ROSTER_FILENAME = "full_roster_4_seasons"       #.csv
+TRAIN_TEST_DS_DIR = PRJ_PATH + "/data/train_test_ds"
+TRAIN_DS_DIR = PRJ_PATH + "/data/train_ds"
+TRAIN_DS_FILENAME = "train_ds_all"              #.p
+TEST_DS_DIR = PRJ_PATH + "/data/test_ds"
+TRAIN_DS_FILENAME = "test_ds_all"               #.p
+MODELRESULT_DIR = PRJ_PATH + "/data/outputs"
+MODELRESULT_FILENAME = "arima_results"          #.p     #Join model type (arima or deepar) string as well as hparam string to this
+'''
+#TODO: change "arima_results" to "model_results"
+
+
+#ASSUMING DATA IS PRESENT IN DATA_DIR
+
+#TODO: make class instance variable (OR variable of singleton object)
+saver_reader = SavingReading()
+
+def preprocessing(data):
+    #assert 'date' in data.columns
+    data['date'] = pd.to_datetime(data['date'])
+    return data 
+
+def generate_list_ds(data, targets, targets_meta, targets_raw, stat_cat_features, dyn_cat_features, dyn_real_features, dyn_real_features_meta, player_names:list, boolTransformed=False):
+    data_meta = glnts.generate_minimal_metadata_all(data, index=None)
+    list_ds = glnts.getListDS(targets if boolTransformed else targets_raw, data_meta,stat_cat_features,dyn_cat_features,dyn_real_features,player_names)
+    return list_ds
+
+
+def load_data_listDS(data,
+                    full_save_dir,
+                    boolSplitTrainTest,
+                    index='date',
+                    feature='cumStatpoints',
+                    forecast_from='2018-10-03',
+                    transform='none',               #TODO: keep? Currently could use in glnts.assembleTargets with stand+scale
+                    roster=None,
+                    column_list=None,
+                    use_exog_feat=False,
+                    boolTransformed=False,
+                    boolSave=False,
+                    stand=False,
+                    scale=True):
+
+    '''
+    Inputs
+    =====
+    data: for ALL players
+
+    Outputs
+    =====
+    x_train_listDS.p + x_test_listDS.p for that player
+    '''
+
+    assert len(data)>0,"Missing data"
+    assert roster is not None and len(roster)>0, "Missing roster"
+    assert all([feat in data.columns for feat in ['name','date']]), f"Missing feature. Data has columns: {data.columns}"
+
+
+    ret = glnts.prep_df(data, \
+                        roster, \
+                        split_from=forecast_from, \
+                        column_list=column_list, \
+                        streamlit=False, \
+                        stand=stand, \
+                        scale=scale, \
+                        boolSplitTrainTest=boolSplitTrainTest \
+                        )
+                      
+    if ret is None: #If any errors in preparing, skip
+        print(f'Error getting data for all players!')
+        return None
+    
+    if boolSplitTrainTest:
+        train, test, targets_trn, targets_test, targets_raw_trn, targets_raw_test, targets_meta_trn, targets_meta_test, targets_raw_meta_trn, targets_raw_meta_test, stat_cat_features_trn, \
+        stat_cat_features_test, dyn_cat_features_trn, dyn_cat_features_test, dyn_real_features_trn, dyn_real_features_test, dyn_real_features_meta_trn, dyn_real_features_meta_test \
+        =  ret 
+
+        #Get names of players with sufficient data
+        player_names = train['name'].unique()
+
+        #COPY FEATURE VERIFICATION GOES HERE.... ARIMA 156:196
+
+        #FEATURE_SELECTION
+        assert all([feat in train.columns for feat in [feature]]), f'Feat missing in these train columns: {train.columns}'
+        assert all([feat in test.columns for feat in [feature]]), f'Feat missing in these test columns: {test.columns}'
+        #TODO: add index change (ARIMA 205:210) if index = 'date' needed
+
+        train_list_ds = generate_list_ds(train,targets_trn, targets_meta_trn, targets_raw_trn, stat_cat_features_trn, dyn_cat_features_trn, dyn_real_features_trn, dyn_real_features_meta_trn, player_names, boolTransformed=boolTransformed)
+        test_list_ds = generate_list_ds(test,targets_test, targets_meta_test, targets_raw_test, stat_cat_features_test, dyn_cat_features_test, dyn_real_features_test, dyn_real_features_meta_test, player_names, boolTransformed=boolTransformed)
+
+        if boolSave:
+            saver_reader.save(train_list_ds,"all_players_trn_ds",full_save_dir,bool_save_s3=False)
+            saver_reader.save(test_list_ds,"all_players_test_ds",full_save_dir,bool_save_s3=False)
+
+        return train_list_ds, test_list_ds
+    else:
+        data, targets, targets_meta, targets_raw, targets_raw_meta, stat_cat_features, dyn_cat_features, dyn_real_features, dyn_real_features_meta = ret
+
+        #Get names of players with sufficient data
+        player_names = data['name'].unique()
+
+        list_ds = generate_list_ds(data, targets, targets_meta, targets_raw, stat_cat_features, dyn_cat_features, dyn_real_features, dyn_real_features_meta, player_names, boolTransformed=boolTransformed)
+        if boolSave:
+            saver_reader.save(list_ds,"all_players_ds",full_save_dir,bool_save_s3=False)
+
+        return list_ds
+
+
+
+
+#TODO: move use_exog_feat to TrainEvaluate.py
+def load_data_main(data_path,roster_path,full_save_dir, boolSplitTrainTest, \
+                    use_exog_feat=False, boolTransformed=False, boolSave=False, \
+                    column_list = ['date', 'name', 'gameNumber', 'cumStatpoints'], stand=False, \
+                    scale=True, index='date',feature='cumStatpoints',forecast_from='2018-10-03'):
+    try:
+        #TODO: replace with saver_reader.read()
+        data = pd.read_csv(data_path)
+        full_roster = pd.read_csv(roster_path)
+        data = preprocessing(data)
+        
+        ret = load_data_listDS(data=data,roster=full_roster, use_exog_feat=use_exog_feat, stand=stand, \
+                                boolTransformed=boolTransformed, boolSave=boolSave, full_save_dir=full_save_dir, \
+                                column_list=column_list,scale=scale,index=index,feature=feature, \
+                                forecast_from=forecast_from,boolSplitTrainTest=boolSplitTrainTest)
+        assert ret is not None, f"Could not load data"
+        if boolSplitTrainTest:
+            train_list_ds, test_list_ds = ret
+            return train_list_ds, test_list_ds
+        else:
+            data_list_ds = ret
+            return data_list_ds
+    
+    except AssertionError as err:
+        print(f'Error in loading data: {err}')
+        return None
