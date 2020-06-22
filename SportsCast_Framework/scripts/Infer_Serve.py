2dc662ac80b610c3c4234350664044494703d242
AlokD123 <alok.deshpande@mail.utoronto.ca>
2020-06-21 21:11:14 -0400

    Updated BentoService packaging to allow for custom save location

--- SportsCast_Framework/scripts/Infer_Serve.py
+++ SportsCast_Framework/scripts/Infer_Serve.py
@@ -1,19 +1,18 @@
 import pandas as pd
-import numpy as np
-import time
-import copy
-from pandas.io.json import json_normalize
-import pickle
 import os
+import logging
 
 from PlayerForecaster import PlayerForecaster
+
 
 import bentoml
 from bentoml.artifact import PickleArtifact
 from bentoml.handlers import DataframeHandler
 
+
+
 @bentoml.artifacts([PickleArtifact('model')])
-@bentoml.env(pip_dependencies=["pandas"])
+@bentoml.env(auto_pip_dependencies=True)
 class Forecast_Service(bentoml.BentoService):
     #self.args = {"player_name":0, "num_games":1}
 
@@ -24,13 +23,23 @@ class Forecast_Service(bentoml.BentoService):
         """        
         return self.artifacts.model.pred_points(series[0],series[1])
 
+def create_service(models_dir:str=os.getcwd()+"/data/models", models_fname:str="model_results", bento_dir:str=os.getcwd()+"/serve/bentoml"):
+    
+    try:
+        # 1) create a model
+        pf = PlayerForecaster(models_dir=models_dir,models_filename=models_fname)
+
+        # 2) `pack` the service with the model
+        bento_service = Forecast_Service()
+        bento_service.pack('model', pf)
+
+        # 3) save  BentoSerivce to file archive
+        saved_path = bento_service.save_to_dir(bento_dir)
+
+        return True
+    except Exception as err:
+        logging.error(f'Could not create service:{err}')
+        return False
+
 if __name__=="__main__":
-    # 1) create a model
-    pf = PlayerForecaster(os.getcwd()+'/./data/outputs/arima_results_m3_fourStep_noFeatures.p')
-
-    # 2) `pack` the service with the model
-    bento_service = Forecast_Service()
-    bento_service.pack('model', pf)
-
-    # 3) save your BentoSerivce to file archive
-    saved_path = bento_service.save()
+    create_service()
