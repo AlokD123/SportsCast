171bc8bdb25661cc15439321c99572897bd1875e
AlokD123 <alok.deshpande@mail.utoronto.ca>
2020-06-22 03:10:21 +0000

    Added setup.py

---
+++ build/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="SportsCast", # Replace with your own username
+    version="0.0.1",
+    author="Alok_Deshpande",
+    author_email="deshpande.alok.1@gmail.com",
+    description="A service for improved sports predictions",
+    #long_description=long_description,
+    #long_description_content_type="text/markdown",
+    url="https://github.com/AlokD123/SportsCast.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
